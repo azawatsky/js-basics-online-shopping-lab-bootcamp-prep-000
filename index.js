1c38a144dc700509b0b688cd0ae523fd3231573d
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-06-01 21:23:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,12 +40,17 @@ function total() {
 }
 
 function removeFromCart(item) {
+  var originalCartLength = cart.length
   for (let i=0;i<cart.length;i++) {
     if (cart[i].itemName===item) {
       cart.splice(i,1)
     }
   }
-  return cart
+  if (originalCartLength===cart.length) {
+    return 'That item is not in your cart.'
+  } else {
+    return cart
+  }
 }
 
 function placeOrder(cardNumber) {
