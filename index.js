d4de0e9893afcd17a40f6a7ae1dfcee9a0c08103
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-06-01 20:57:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ function viewCart() {
   if (cart.length===0) {
     return 'Your shopping cart is empty.'
   } else if (cart.length===1) {
-    return `In your cart, you have ${cart[0].itemName} at $ ${cart[0].itemPrice}.`
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   } else {
     var message = 'In your cart, you have'
     for (let i=0;i<cart.length-1;i++) {
-      message = `${message} ${cart[i].itemName} at $ ${cart[i].itemPrice},`
+      message = `${message} ${cart[i].itemName} at $${cart[i].itemPrice},`
     }
     var lastItem = cart.slice(-1)
-    message = `${message} and ${cart.slice(-1)[0].itemName} at ${cart.slice(-1)[0].itemPrice}.`
+    message = `${message} and ${cart.slice(-1)[0].itemName} at $${cart.slice(-1)[0].itemPrice}.`
     return message
   }
 }
